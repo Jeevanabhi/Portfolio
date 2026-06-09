8551dbf2fc43706e78fdecc13e4c41dc104cc6f7
Jeevanabhi <jeevanjenin@gmail.com>
2026-06-09 12:31:16 +0530

    Convert serverless function to CommonJS for Vercel compat

--- api/submit.js
+++ api/submit.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Method not allowed' });
   }
@@ -31,4 +31,4 @@ export default async function handler(req, res) {
   } catch (error) {
     return res.status(500).json({ success: false, message: error.message });
   }
-}
+};
