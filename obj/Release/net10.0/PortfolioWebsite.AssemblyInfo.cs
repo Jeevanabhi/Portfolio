05ab9ff0ef67c4e2bc7311718f12e5c678106ac8
Jeevanabhi <jeevanjenin@gmail.com>
2026-06-09 12:09:25 +0530

    Fix integrity hash mismatch by treating _framework assets as binary

--- obj/Release/net10.0/PortfolioWebsite.AssemblyInfo.cs
+++ obj/Release/net10.0/PortfolioWebsite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cb647711424834026391df0d20983222eb644bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3c4829e211e89655fdd827ada33646f705f01a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortfolioWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
